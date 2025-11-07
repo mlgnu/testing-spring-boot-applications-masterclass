d2741ef28911ce7756fe7ca8cf2d2e2ad3bf831c
mlgnu <mohamedtahahelmy@gmail.com>
2025-11-07 09:47:22 +0800

    test: add unit tests for JsonTest class with JsonAssert and JsonPath

--- src/test/java/de/rieckpil/courses/book/JsonTest.java
+++ src/test/java/de/rieckpil/courses/book/JsonTest.java
@@ -1,18 +1,28 @@
 package de.rieckpil.courses.book;
 
+import com.jayway.jsonpath.JsonPath;
 import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class JsonTest {
 
   @Test
   void testWithJSONAssert() throws JSONException {
     String result = "{\"name\": \"duke\", \"age\":\"42\", \"hobbies\": [\"soccer\", \"java\"]}";
+    JSONAssert.assertEquals("{\"name\": \"duke\"}", result, false);
   }
 
   @Test
   void testWithJsonPath() {
     String result =
         "{\"age\":\"42\", \"name\": \"duke\", \"tags\":[\"java\", \"jdk\"], \"orders\": [42, 42, 16]}";
+
+    Assertions.assertEquals(2,
+      JsonPath.parse(result).read("$.tags.length()", Long.class));
+
+    Assertions.assertEquals("duke",
+      JsonPath.parse(result).read("$.name", String.class));
   }
 }
