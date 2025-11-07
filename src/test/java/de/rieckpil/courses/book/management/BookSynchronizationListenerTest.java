bb823a433dceabfcde06bbe9adb00f409a129cea
mlgnu <mohamedtahahelmy@gmail.com>
2025-11-07 08:43:24 +0800

    test: add unit tests with mocks for BookSynchronizationListener class

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -1,10 +1,17 @@
 package de.rieckpil.courses.book.management;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +28,58 @@ class BookSynchronizationListenerTest {
   @Captor private ArgumentCaptor<Book> bookArgumentCaptor;
 
   @Test
-  void shouldRejectBookWhenIsbnIsMalformed() {}
+  @DisplayName("should reject book when ISBN is malformed")
+  void shouldRejectBookWhenIsbnIsMalformed() {
+    BookSynchronization bookSynchronization = new BookSynchronization("32");
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient, bookRepository);
+  }
 
   @Test
-  void shouldNotOverrideWhenBookAlreadyExists() {}
+  @DisplayName("shouldn't override when book already exists")
+  void shouldNotOverrideWhenBookAlreadyExists() {
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(new Book());
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient);
+    verify(bookRepository, times(0)).save(ArgumentMatchers.any());
+  }
 
   @Test
-  void shouldThrowExceptionWhenProcessingFails() {}
+  @DisplayName("should throw exception when processing fails")
+  void shouldThrowExceptionWhenProcessingFails() {
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenThrow(new RuntimeException("Network timeout"));
+
+    Assertions.assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
+  }
 
   @Test
-  void shouldStoreBookWhenNewAndCorrectIsbn() {}
+  @DisplayName("should store book when new and correct ISBN")
+  void shouldStoreBookWhenNewAndCorrectIsbn() {
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+
+    Book requestedBook = new Book();
+    requestedBook.setTitle("Head First Design Patterns");
+    requestedBook.setIsbn(VALID_ISBN);
+
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenReturn(requestedBook);
+    when(bookRepository.save(ArgumentMatchers.any())).then(invocation -> {
+        Book methodArgument = invocation.getArgument(0);
+        methodArgument.setId(1L);
+        return methodArgument;
+      });
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verify(bookRepository.save(bookArgumentCaptor.capture()));
+
+    Book methodArgument = bookArgumentCaptor.getValue();
+    assertEquals(VALID_ISBN, methodArgument.getIsbn());
+    assertEquals("Head First Design Patterns", methodArgument.getTitle());
+  }
 }
