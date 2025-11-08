811974fbcabbd1a2f9d29ca7a50f9c7398977a29
mlgnu <mohamedtahahelmy@gmail.com>
2025-11-08 09:17:57 +0800

    test: add integration tests for BookController using MockMvc

--- src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
@@ -1,13 +1,25 @@
 package de.rieckpil.courses.book.management;
 
 import de.rieckpil.courses.config.WebSecurityConfig;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BookController.class)
@@ -21,13 +33,49 @@ class BookControllerTest {
   @Autowired private MockMvc mockMvc;
 
   @Test
-  void shouldGetEmptyArrayWhenNoBooksExists() throws Exception {}
+  @DisplayName("should get empty array when no book exists")
+  void shouldGetEmptyArrayWhenNoBooksExists() throws Exception {
+    MvcResult mvcResult = this.mockMvc
+      .perform(MockMvcRequestBuilders.get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", Matchers.is(0)))
+      .andDo(MockMvcResultHandlers.print())
+      .andReturn();
+  }
 
   @Test
-  void shouldNotReturnXML() throws Exception {}
+  @DisplayName("shouldn't return XML")
+  void shouldNotReturnXML() throws Exception {
+    MvcResult mvcResult = this.mockMvc
+      .perform(MockMvcRequestBuilders.get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML))
+      .andExpect(status().isNotAcceptable())
+      .andReturn();
+  }
 
   @Test
-  void shouldGetBooksWhenServiceReturnsBooks() throws Exception {}
+  void shouldGetBooksWhenServiceReturnsBooks() throws Exception {
+
+    Book book1 = createBook(1L, "42", "Java 22", "Taha", "Master Java",
+      "Software Engineering", 200L, "Oracle", "https://java.com/22");
+
+    Book book2 = createBook(2L, "43", "Java 25", "Taha", "Master Java",
+      "Software Engineering", 200L, "Oracle", "https://java.com/25");
+
+    when(bookManagementService.getAllBooks()).thenReturn(List.of(book1, book2));
+
+    this.mockMvc
+      .perform(MockMvcRequestBuilders.get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", Matchers.is(2)))
+      .andExpect(jsonPath("$.[0].isbn", Matchers.is("42")))
+      .andExpect(jsonPath("$.[0].title", Matchers.is("Java 22")))
+      .andExpect(jsonPath("$.[1].isbn", Matchers.is("43")))
+      .andExpect(jsonPath("$.[1].title", Matchers.is("Java 25")))
+      .andReturn();
+  }
 
   private Book createBook(
       Long id,
